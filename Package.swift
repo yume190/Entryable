dacd8b42daecf77059eba79dbfa7e6b9cf25f96a
yume190 <yume190@gmail.com>
2018-04-03 16:59:51 +0800

    SPM 支持 test 中

--- Package.swift
+++ Package.swift
@@ -2,6 +2,9 @@ import PackageDescription
 
 let package = Package(
     name: "YumeAlamofire",
+    products: [
+        .library(name: "YumeAlamofire", type: .static, targets: ["YumeAlamofire"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.4.0"),
         .package(url: "https://github.com/yume190/JSONDecodeKit.git", from: "4.0.1"),
