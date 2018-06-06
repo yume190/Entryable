f546bf4b372da3e44f1bb7489dbc97eafb3da791
yume190 <yume190@gmail.com>
2018-06-06 17:20:05 +0800

    fix seal reject error

--- Sources/YumeAlamofire/Entryable+Await.swift
+++ Sources/YumeAlamofire/Entryable+Await.swift
@@ -17,12 +17,16 @@ extension Entryable where ResponseType: Codable {
             // Make an HTTP request to download the image.
             self.dataRequest.responseData { (res) in
                 let decoder = JSONDecoder()
-                if
-                    let data = res.result.value,
-                    let result = try? decoder.decode(ResponseType.self, from: data) {
-                    seal.fulfill(result)
+                
+                if let data = res.result.value {
+                    do {
+                        let result = try decoder.decode(ResponseType.self, from: data)
+                        seal.fulfill(result)
+                    } catch {
+                        seal.reject(error)
+                    }
                 } else {
-                    seal.reject(res.error!)
+                    seal.reject(res.error ?? NSError(domain: "no response data", code: 0, userInfo: nil))
                 }
             }
         }
@@ -34,12 +38,15 @@ extension Entryable where ResponseType: JSONDecodable {
         return Promise<ResponseType> { (seal) in
             // Make an HTTP request to download the image.
             self.dataRequest.responseData { (res) in
-                if
-                let data = res.result.value,
-                let result = try? ResponseType.decode(json: JSON(data: data)) {
-                    seal.fulfill(result)
+                if let data = res.result.value {
+                    do {
+                        let result = try ResponseType.decode(json: JSON(data: data))
+                        seal.fulfill(result)
+                    } catch {
+                        seal.reject(error)
+                    }
                 } else {
-                    seal.reject(res.error!)
+                    seal.reject(res.error ?? NSError(domain: "no response data", code: 0, userInfo: nil))
                 }
             }
         }
