edc009e0f35d425eb8091b9a698db071bfac5416
Yume <yume190@gmail.com>
2023-04-20 11:51:03 +0800

    add new config

--- Sources/Entryable/NetError.swift
+++ Sources/Entryable/NetError.swift
@@ -7,23 +7,33 @@
 
 import Foundation
 
+public private(set) var isShowDetailError: Bool = false
+public func setup(isShowDetailError isShow: Bool) {
+    isShowDetailError = isShow
+}
+
 public enum NetError: Error, CustomStringConvertible {
     case url(HTTPRawResponse<Data>, Error)
     
     public var description: String {
         switch self {
-        case let .url(raw, error): return """
-        [NET ERROR]
-        request:
-            url: \(raw.request?.url?.description ?? "")
-            method: \(raw.request?.method?.rawValue ?? "")
-            header: \(raw.request?.headers.description ?? "")
-            body: \(raw.request?.httpBody?.utf8 ?? "")
-        response:
-            header: \(raw.response?.headers.description ?? "")
-            body: \(raw.data.utf8 ?? "")
-        error: \(error)
-        """
+        case let .url(raw, error):
+            guard isShowDetailError else {
+                return "\(error)"
+            }
+            
+            return """
+            [NET ERROR]
+            request:
+                url: \(raw.request?.url?.description ?? "")
+                method: \(raw.request?.method?.rawValue ?? "")
+                header: \(raw.request?.headers.description ?? "")
+                body: \(raw.request?.httpBody?.utf8 ?? "")
+            response:
+                header: \(raw.response?.headers.description ?? "")
+                body: \(raw.data.utf8 ?? "")
+            error: \(error)
+            """
         }
     }
 }
